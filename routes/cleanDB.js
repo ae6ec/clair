4c698deb9e6a6987cdc72553bb7c577e3585d557
Alok <alok@navigus.in>
2021-07-11 00:08:48 +0530

    fixed unhandledpromiserejectionwarning in cleanDB

--- routes/cleanDB.js
+++ routes/cleanDB.js
@@ -9,27 +9,32 @@ router.get('/', async (request,response) => {
 
     const MS_PER_HOUR = 1000 * 60 * 60 ;
     let error = "";
-    const postalRef = await db.collection('postal');
-    var allfiles = postalRef.get()
-        .then(snapshot => {
-            snapshot.forEach(file => {
-    
-                const hoursdiff = Math.floor((now - file.data().createdAt.toDate())/MS_PER_HOUR);
-                console.log(`${file.id} => `,file.data() ,hoursdiff, 'Created before (in Hours)');
-                
-                if (hoursdiff >= 24){
-                    console.log(`${file.id} => ${hoursdiff} getting deleted`);
-                    (async function(){
-                        const k = await db.collection('postal').doc(file.id).delete();
-                    })()
-                }
-    
-            });
-        })
-        .catch(err => {
+    try {
+        const postalRef = await db.collection('postal');
+        var allfiles = postalRef.get()
+            .then(snapshot => {
+                snapshot.forEach(file => {
+        
+                    const hoursdiff = Math.floor((now - file.data().createdAt.toDate())/MS_PER_HOUR);
+                    console.log(`${file.id} => `,file.data() ,hoursdiff, 'Created before (in Hours)');
+                    
+                    if (hoursdiff >= 24){
+                        console.log(`${file.id} => ${hoursdiff} getting deleted`);
+                        (async function(){
+                            const k = await db.collection('postal').doc(file.id).delete().catch(err => {
+                                `Error while Deleting Document: ${err}`
+                            })
+                        })()
+                    }
+        
+                })
+            })
+
+        } catch(err){
             console.log('Error retrieving/Deleting documents: ', err);
             error = err;
-        });
+        }
+    
 
     response.json({ 
         status : "successfully Triggered",      
